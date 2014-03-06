920a49fff9e0c21d0557c80115e907576f6276e9
zuluaica18 <zuluaica18@gmail.com>
2014-03-05 22:01:08 -0500

    Ingresar valores por consola

--- src/com/fyp/guice/example/application/Start.java
+++ src/com/fyp/guice/example/application/Start.java
@@ -1,5 +1,7 @@
 package com.fyp.guice.example.application;
 
+import java.io.*;
+
 import com.fyp.guice.example.bindings.*;
 import com.fyp.guice.example.domain.*;
 import com.fyp.guice.example.domain.interfaces.*;
@@ -8,33 +10,53 @@ import com.google.inject.*;
 public class Start{
 	
 	public static void main(String[] args) throws ClassNotFoundException {
+		
+		// Leer tipo de tarjeta
+		BufferedReader lectura = new BufferedReader(new InputStreamReader(System.in));
+		int inCardType = 0;
+		System.out.println("Ingrese su tipo de tarjeta: ");
+		try {
+			inCardType = Integer.parseInt(lectura.readLine());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	    /*
 	     * Guice.createInjector() takes your Modules, and returns a new Injector
 	     * instance. Most applications will call this method exactly once, in their
 	     * main() method.
 	     */
 		Injector injector;
-	    CardType cardType = CardType.Falabella;
+		
+		CardType cardType = null;
+		if (inCardType == 0) cardType = CardType.Paypal;
+		else if (inCardType == 1) cardType = CardType.GoogleWallet;
+		else cardType = CardType.Falabella;
 
-	    if (args[0] != null) {
+	    if (args.length > 0) {
 			injector = Guice.createInjector(new BillingModule((Class<? extends CreditCardProcessor>)Class.forName(args[0])));
 		}
 		else{
     	    switch (cardType) {
     			case Paypal:
     				injector = Guice.createInjector(new BillingModule(PaypalCreditCardProcessor.class));
+    				System.out.println("Tarjeta ingresada: Paypal");
     				break;
     
     			case GoogleWallet:
     				injector = Guice.createInjector(new BillingModule(GoogleWalletProcessor.class));
+    				System.out.println("Tarjeta ingresada: GoogleWallet");
     				break;
     				
     			case Falabella:
     				injector = Guice.createInjector(new BillingModule(FalabellaCreditCardProcessor.class));
+    				System.out.println("Tarjeta ingresada: Falabella");
     				break;
     				
     			default:			
     				injector = Guice.createInjector(new BillingModule(PaypalCreditCardProcessor.class));
+    				System.out.println("Tarjeta no valida");
     				break;
 			}	    
 		}
